7caf64a82655ed842fd3924697105fc059994d6b
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2023-08-23 18:20:10 -0300

    add more api test

--- test/integration/test_api.py
+++ test/integration/test_api.py
@@ -1,23 +1,37 @@
 import requests
 
+from typing import Dict
+from http import HTTPStatus
 from unittest import TestCase
 
 
 class TestApi(TestCase):
 
-    base_url = 'http://localhost:8080'
+    endpoint = 'http://localhost:8080'
 
     def test_insert_food(self):
-        pass
+        # Given
+        content = {
+            "name": "My food",
+            "description": "This is the description",
+            "link": "https://fake-link.com",
+            "link_image": "https://fake-image-link.com"
+        }
+        # When
+        endpoint = self.base_url + '/api/food/'
+        response = requests.post(endpoint, data=content)
+        # Then
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertEqual(response.content, [])
 
     def test_get_food_by_name(self):
         pass
 
     def test_get_all_foods(self):
         # Given
-        endpoint = self.base_url + '/api/food/'
         # When
+        endpoint = self.base_url + '/api/food/'
         response = requests.get(endpoint)
-        print(response)
         # Then
-        self.assertEqual(response, '')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIsInstance(response.content, Dict)
