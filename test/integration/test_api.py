3f97193ee1d3ee8a7be79fba6a144b09e6fe5b08
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2023-08-23 16:28:30 -0300

    remove images

---
+++ test/integration/test_api.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestIntegrationApi(TestCase):
+    def test_insert_food(self):
+        pass
+
+    def test_get_food_by_name(self):
+        pass
+
+    def test_get_all_foods(self):
+        pass
